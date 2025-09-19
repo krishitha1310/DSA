6745dfd14d695bb3f45625ad0678fb008b0f911f
krishitha1310 <krishitha174@gmail.com>
2025-09-19 11:39:09 +0530

    Time: 1 ms (99.85%), Space: 46.7 MB (10.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       int r=nums[0];
+        for(int i=1;i<nums.length;i++){
+             r=r^nums[i];
+        }
+        return r;
+    }
+}
