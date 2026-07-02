93f73a713f986b094ab26e9bca520fbe03c9333a
krishitha1310 <krishitha174@gmail.com>
2026-07-02 20:23:41 +0530

    Time: 32 ms (5.25%), Space: 47.4 MB (80.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,29 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] row=new int[matrix.length*matrix[0].length];
-        int[] col=new int[matrix.length*matrix[0].length];
-        int x=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==0){
-                    row[x]=i;
-                    col[x]=j;
+        int[] row = new int[matrix.length * matrix[0].length];
+        int[] col = new int[matrix.length * matrix[0].length];
+        int x = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[x] = i;
+                    col[x] = j;
                     x++;
                 }
             }
         }
-        int ind=0;
-        for(int k=0;k<x;k++){
+        int ind = 0;
+        for (int k = 0; k < x; k++) {
             //complete row change
-            for(int l=0;l<matrix[row[ind]].length;l++){
-                matrix[row[k]][l]=0;
+            for (int l = 0; l < matrix[row[ind]].length; l++) {
+                matrix[row[k]][l] = 0;
             }
-           
+
             //complete column change
-            for(int m=0;m<matrix.length;m++){
-                matrix[m][col[k]]=0;
+            for (int m = 0; m < matrix.length; m++) {
+                matrix[m][col[k]] = 0;
             }
-         
+
         }
         System.out.println(Arrays.toString(matrix));
     }
