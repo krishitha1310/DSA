3a355cfdd9cce53f82bc116aa9073f65c6bf7646
krishitha1310 <krishitha174@gmail.com>
2026-07-20 22:41:20 +0530

    Time: 4 ms (97.01%), Space: 47.1 MB (79.54%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int[] arr=new int[grid.length*grid[0].length];
+        int x=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                arr[x++]=grid[i][j];
+            }
+        }
+        k=k%arr.length;
+        int l=arr.length-k,r=arr.length-1;
+        while(l<r){
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+        }
+        l=0;r=arr.length-k-1;
+        while(l<r){
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+        }
+        l=0;r=arr.length-1;
+       while(l<r){
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+       }
+        List<List<Integer>> list=new ArrayList<>();
+        x=0;
+        int len=0;
+        while(len<arr.length){
+            List<Integer> lis=new ArrayList<>();
+            l=0;
+            while(l<grid[0].length){
+                lis.add(arr[x++]);
+                l++;
+                len++;
+                if(x==arr.length)break;
+            } 
+            list.add(lis);
+       }
+       return list;
+    }
+}
