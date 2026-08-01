7e77b9e9518d2fbbb4317a7c788f547c178df973
krishitha1310 <krishitha174@gmail.com>
2026-08-02 01:10:25 +0530

    Time: 13 ms (19.27%), Space: 47.3 MB (35.82%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> rad=new LinkedList<>();
+        Queue<Integer> dir=new LinkedList<>();
+        for(int i=0;i<senate.length();i++){
+            char ch=senate.charAt(i);
+            if(ch=='R'){
+                rad.offer(i);
+            }
+            else{
+                dir.offer(i);
+            }
+        }
+        while(!rad.isEmpty()&&!dir.isEmpty()){
+            int d=dir.poll(),r=rad.poll();
+            if(r<d){
+                rad.offer(r+senate.length());
+            }
+            else{
+                dir.offer(d+senate.length());
+            }
+        }
+        if(rad.isEmpty())return "Dire";
+        return "Radiant";
+    }
+}
