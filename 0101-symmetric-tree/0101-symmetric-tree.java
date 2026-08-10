b2d6b4dba8fc13aae855e4a2e95b34998601cd69
krishitha1310 <krishitha174@gmail.com>
2026-08-10 21:23:02 +0530

    Time: 1 ms (8.36%), Space: 43.7 MB (34.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,39 +15,40 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size(),x=0;
-            int[] arr=new int[size*2];
+        while (!q.isEmpty()) {
+            int size = q.size(), x = 0;
+            int[] arr = new int[size * 2];
 
-            for(int i=0;i<size;i++){
-                TreeNode temp=q.poll();
-                if(temp.left!=null){
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                if (temp.left != null) {
                     q.offer(temp.left);
                 }
-                if(temp.right!=null){
+                if (temp.right != null) {
                     q.offer(temp.right);
                 }
 
-                if(temp.left==null){
-                    arr[x++]=-101;
-                }else{
-                    arr[x++]=temp.left.val;
+                if (temp.left == null) {
+                    arr[x++] = -101;
+                } else {
+                    arr[x++] = temp.left.val;
                 }
-                if(temp.right==null){
-                    arr[x++]=-101;
-                }else{
-                    arr[x++]=temp.right.val;
+                if (temp.right == null) {
+                    arr[x++] = -101;
+                } else {
+                    arr[x++] = temp.right.val;
                 }
             }
 
-            int j=0,k=arr.length-1;
-            while(j<k){
-                if(arr[j]!=arr[k]){
+            int j = 0, k = arr.length - 1;
+            while (j < k) {
+                if (arr[j] != arr[k]) {
                     return false;
                 }
-                j++;k--;
+                j++;
+                k--;
             }
         }
         return true;
