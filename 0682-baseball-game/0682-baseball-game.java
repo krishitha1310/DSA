36c3623a0f8741c17aa299280854384ccca45271
krishitha1310 <krishitha174@gmail.com>
2026-08-27 23:58:23 +0530

    Fix formatting of Stack initialization

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer> st=new Stack<>();
+        Stack<Integer> st =new Stack<>();
         for(int i=0;i<operations.length;i++){
             if(operations[i].equals("D")){
                 st.push(st.peek()*2);
