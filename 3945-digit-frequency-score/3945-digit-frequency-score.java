bca98a4e9cc27070bdc930fd0e56e908b229331f
krishitha1310 <krishitha174@gmail.com>
2026-08-28 23:11:56 +0530

    Fix formatting of HashMap initialization

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,6 +1,6 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-       HashMap<Integer,Integer> hm=new HashMap<>();
+       HashMap<Integer,Integer> hm = new HashMap<>();
        int sum=0;
        while(n!=0){
         hm.put(n%10,hm.getOrDefault(n%10,0)+1);
