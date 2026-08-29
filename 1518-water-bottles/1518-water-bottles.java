4d3f4fbee3a77b585df7d4bde25d74dacf4537ae
krishitha1310 <krishitha174@gmail.com>
2026-08-29 23:18:36 +0530

    Fix spacing issue in while loop condition

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int drunk=numBottles,empty=numBottles;
-        while(empty>=numExchange){
+        while(empty> = numExchange){
             int neww=empty/numExchange;
             drunk+=neww;
             empty=neww+(empty%numExchange);
