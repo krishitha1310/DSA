25594df4a85500364a04df8cc30d2a35286274fc
krishitha1310 <krishitha174@gmail.com>
2026-08-30 23:23:49 +0530

    Fix formatting of traffic signal condition

--- 3894-traffic-signal-color/3894-traffic-signal-color.java
+++ 3894-traffic-signal-color/3894-traffic-signal-color.java
@@ -2,7 +2,7 @@ class Solution {
     public String trafficSignal(int timer)  {
         if(timer==0) return "Green";
         else if(timer==30) return "Orange";
-        else if(timer> 30&&timer<=90) return "Red";
+        else if(timer> 30&& timer<=90) return "Red";
         return "Invalid";
     }
 }
