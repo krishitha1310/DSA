d6463f0548ee5c649e3bf6a60e967d63cad3740c
krishitha1310 <krishitha174@gmail.com>
2026-09-24 23:27:43 +0530

    Fix formatting of HashMap initialization

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        HashMap<Integer,String> hm=new HashMap<>();
+        HashMap<Integer,String> hm =new HashMap<>();
         for(int i=0;i<heights.length;i++){
             hm.put(heights[i],names[i]);
         }
